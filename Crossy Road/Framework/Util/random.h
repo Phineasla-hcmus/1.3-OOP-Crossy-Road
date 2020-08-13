45d99c5dc8fcc3a20eef67c746bcfe65d572161b
MinhLe2512 <63902542+MinhLe2512@users.noreply.github.com>
2020-08-13 17:10:04 +0700

    test

---
+++ Crossy Road/Framework/Util/random.h
@@ -0,0 +1,22 @@
+#ifndef _random
+#define _random
+#include<ctime>
+#include<random>
+class random
+{
+private:
+	std::mt19937 engine;
+	template<typename Distribution, typename T>
+	T get_number(T low, T high) {
+		Distribution dist(low, high);
+		return dist(engine);
+	}
+public:
+	using uniform_int = std::uniform_int_distribution<>;
+	using uniform_double = std::uniform_real_distribution<>;
+	random(unsigned seed = std::time(nullptr));
+	int int_in_range(int low, int high);
+	double double_in_range(double low, double high);
+};
+#endif // !_random
+
