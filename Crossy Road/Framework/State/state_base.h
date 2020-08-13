6b9edbc6a37a0210c9ab92584a185cb1b21c3b7f
Phineasla <63248715+Phineasla@users.noreply.github.com>
2020-08-13 22:32:18 +0700

    game constructor

--- Crossy Road/Framework/State/state_base.h
+++ Crossy Road/Framework/State/state_base.h
@@ -1,10 +1,16 @@
 #ifndef _state_base
 #define _state_base
+#include"SFML/Graphics.hpp"
 class state_base
 {
 private:
 
 public:
+	state_base();
+	virtual ~state_base();
+
+	virtual void update(sf::Time delta_time) = 0;
+	virtual void render() = 0;
 
 };
 #endif // !_state_base
--- Crossy Road/Framework/game.cpp
+++ Crossy Road/Framework/game.cpp
@@ -1,4 +1,14 @@
 #include "game.h"
-game::game() {
+game::game()
+	: _window(sf::VideoMode(1280, 720), "Crossy Road")
+{
+	_window.setFramerateLimit(60);
+	//set icon for window, nothing happen if failed
+	sf::Image icon;
+	if (icon.loadFromFile("resource/icon.png"))
+		_window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+}
 
+void game::run()
+{
 }
