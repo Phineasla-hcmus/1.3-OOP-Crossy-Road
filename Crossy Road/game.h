7ae5f5cfb26fd23c9b9a5a0bfdbd68f3f55a69ad
Phineasla <63248715+Phineasla@users.noreply.github.com>
2020-08-13 11:26:53 +0700

    VERY basic framework

---
+++ Crossy Road/game.h
@@ -0,0 +1,16 @@
+#ifndef _main_game
+#define _main_game
+#include<stack>
+#include<vector>
+#include<SFML/Graphics.hpp>
+
+class game
+{
+private:
+public:
+	game(const game&)	= delete;
+	game(game&&)		= delete;
+	game();
+};
+#endif // !_main_game
+
