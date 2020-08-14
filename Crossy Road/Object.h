08d1cf9b0222ef6b96a759cfc49ee6974b0ace3b
MinhLe2512 <63902542+MinhLe2512@users.noreply.github.com>
2020-08-14 22:58:42 +0700

    Again

--- Crossy Road/Object.h
+++ Crossy Road/Object.h
@@ -1,4 +1,5 @@
 #pragma once
+<<<<<<< Updated upstream
 
 #include <SFML/Graphics.hpp>
 #include "Collidable.h"
@@ -26,12 +27,42 @@ public:
     void makeAlive()
     {
         m_isAlive = true;
+=======
+#ifndef OBJ_H
+#define OBJ_H
+#include <SFML\Graphics.hpp>
+#include "Crossy Road/Collider.h"
+
+class Object : public Collider
+{
+public:
+    enum class Type
+    {
+        Animal, Car, Bus,tree
+    };
+
+    Object(const sf::Vector2f& initialLocation);
+
+    void move(float x);
+
+    const sf::Vector2f& getPosition() const;
+    bool isActive() const;
+
+
+    void onCollide(Collider& other) override;
+
+    Type getType() const;
+    void makeAlive()
+    {
+        m_isActive = true;
+>>>>>>> Stashed changes
         m_location = m_initialPosition;
     }
 
 private:
     const sf::Vector2f m_initialPosition;
     sf::Vector2f m_location;
+<<<<<<< Updated upstream
     bool m_isAlive = false;
  
    /* Type m_type;*/
@@ -42,3 +73,13 @@ public:
     constexpr static float HEIGHT = 32.f;
     sf::RectangleShape rect;
 };
+=======
+    bool m_isActive = false;
+    RectangleShape body;
+
+public:
+    constexpr static float WIDTH = 48;
+    constexpr static float HEIGHT = 32;
+};
+#endif
+>>>>>>> Stashed changes
