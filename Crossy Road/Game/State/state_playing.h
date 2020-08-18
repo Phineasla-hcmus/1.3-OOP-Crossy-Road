e62cf4c9c9f3e52aed41832c601cdaf83d0cbff0
Trần Quốc Tuấn <56530679+19127650@users.noreply.github.com>
2020-08-18 20:18:57 +0700

    revert

--- Crossy Road/Game/State/state_playing.h
+++ Crossy Road/Game/State/state_playing.h
@@ -1,66 +1,7 @@
-#pragma once
-
-#include "../../Framework/State/state_base.h"
+#ifndef _state_playing
+#include"../../Framework/State/state_base.h"
 #include "../World.h"
-#include "../../Framework/GUI/StackMenu.h"
 
-
-
- class state_playing : public state_base
-    {
-    public:
-        state_playing(game& game);
-
-        void handleEvent(sf::Event e)                   override;
-        void handleInput()                              override;
-        void update(sf::Time deltaTime)            override;
-        void render(sf::RenderTarget& renderer)    override;
-
-    private:
-        World m_world;
-
-        int m_score = 0;
-        bool m_isGameover;
-        sf::Clock m_gameOverClock;
-
-        StackMenu m_gameOverMenu;
-
-        //Displays how many lives the player has left
-        class LifeDisplay
-        {
-        public:
-            LifeDisplay();
-
-            void draw(sf::RenderTarget& window, int lives);
-
-        private:
-            Widget::Text m_label;
-            sf::RectangleShape m_lifeStamp;
-        } m_lifeDisplay;
-
-        //Displays the Player's current score
-        class ScoreDisplay
-        {
-        public:
-            ScoreDisplay(float centreX, const std::string& text);
-            
-
-            void update(int newScore);
-
-            void draw(sf::RenderTarget& target);
-
-            int getCurrentScoreDisplayed() const;
-        private:
-            void updateDisplay();
-
-<<<<<<< HEAD
-            Widget::Text m_label;
-            std::string m_text;
-            int m_currentScore;
-            float m_centerPosition;
-        } m_scoreDisplay, m_highestScoreDisplay;
-    };
-=======
 class state_playing :public state_base {
 private:
 	World _world;
@@ -73,11 +14,4 @@ public:
 	void update(sf::Time delta_time)		override;
 	void draw(sf::RenderTarget& renderer)	override;
 };
-#endif // !_state_playing
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of ff83bed... backup state playing
-=======
->>>>>>> parent of ff83bed... backup state playing
-=======
->>>>>>> parent of ff83bed... backup state playing
+#endif // !_state_playing/
