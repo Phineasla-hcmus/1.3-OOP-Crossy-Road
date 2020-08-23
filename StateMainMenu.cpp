6f3c5b5fdb4d58e64ff1e6db40b9b5036ea6f9f0
Truong19clc5 <nguyentony3867@gmail.com>
2020-08-23 17:53:28 +0700

    not check

---
+++ StateMainMenu.cpp
@@ -0,0 +1,63 @@
+#include "StateMainMenu.h"
+#include"Game.h"
+#include<SFML\Graphics.hpp>
+#include"Button.h"
+#include"resource.h"
+#include"PauseMenu.h"
+StateMainMenu::StateMainMenu(Game& game)
+    : StateBase(game)
+    , m_mainMenu(game.getWindow(), Display::HEIGHT / 2 - 100)
+{
+    m_banner.setSize({ 800, 100 });
+    m_banner.setFillColor(sf::Color::Yellow);
+    m_banner.setTexture(&resource::get().textures.get("si/crossyroad","png"));
+    m_banner.setPosition(210, 30);
+  
+    auto playBtn = makeButton();
+    playBtn->setText("Play game");
+    playBtn->setFunction([&]() {
+       m_pGame->pushState<state_playing>(*m_pGame);
+        });
+
+    auto LoadGameBtn = makeButton();
+    LoadGameBtn->setText("Load Game");
+    LoadGameBtn->setFunction([&]() {
+       m_pGame->pushState<PauseMenu>(*m_pGame);
+        //m_pGame->exit = true;
+        });
+
+    auto exitBtn = makeButton();
+    exitBtn->setText("Exit game");
+    exitBtn->setFunction([&]() {
+        m_pGame->exit = true;
+        });
+
+    m_mainMenu.addWidget(std::move(playBtn));
+    m_mainMenu.addWidget(std::move(LoadGameBtn));
+    m_mainMenu.addWidget(std::move(exitBtn));
+    m_mainMenu.setTitle("Choose  Action", game.getWindow());
+}
+
+void StateMainMenu::setPosition(const sf::Vector2f position)
+{
+    m_container.setPosition(position);
+   m_thumbnail.setPosition(position);
+}
+void StateMainMenu::handleEvent(sf::Event e)
+{
+    m_mainMenu.handleEvent(e, m_pGame->getWindow());
+}
+void StateMainMenu::draw(sf::RenderTarget& renderer)
+{
+   renderer.draw(m_container);
+    renderer.draw(m_thumbnail);
+    m_mainMenu.render(renderer);
+    renderer.draw(m_banner);
+}
+//void StateMainMenu::render(sf::RenderTarget& renderer)
+//{
+//    //m_background.draw(renderer);
+//    m_mainMenu.render(renderer);
+//    renderer.draw(m_banner);
+//
+//}
