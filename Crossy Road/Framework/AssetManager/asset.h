08f75391063adfc81e161cd355148787f4f8f343
Phineasla <63248715+Phineasla@users.noreply.github.com>
2020-08-24 09:59:03 +0700

    Update asset.h

--- Crossy Road/Framework/AssetManager/asset.h
+++ Crossy Road/Framework/AssetManager/asset.h
@@ -10,11 +10,12 @@ class asset : public no_copy
 {
 private:
 	asset();
-	static asset& get();
+
 	asset_manager<sf::Font>			_font;			//fonts folder
 	asset_manager<sf::Texture>		_texture;		//textures folder
 	asset_manager<sf::SoundBuffer>	_sound_buffer;	//audio folder
-public:
+public:	
+	static asset& get();
 	static asset_manager<sf::Font>			font();
 	static asset_manager<sf::Texture>		texture();
 	static asset_manager<sf::SoundBuffer>	sound();
