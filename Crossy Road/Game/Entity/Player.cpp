3a85c968e21bf038dfc21179971c163547a1f384
Phineasla <63248715+Phineasla@users.noreply.github.com>
2020-08-24 09:48:54 +0700

    test some smooth movement

--- Crossy Road/Game/Entity/Player.cpp
+++ Crossy Road/Game/Entity/Player.cpp
@@ -1,85 +1,83 @@
 #include "Player.h"
 #include "../world.h"
 
-    Player::Player() :Collision(90.f,90.f)
-    {
-        sf::Vector2f size_player = { 90.f,90.f };
-        people.setSize(size_player);        
-        people.setPosition(this->origin_pos);
-       // people.setTextureRect({ 0, 0, 11, 8 });
+Player::Player() :Collision(90.f,90.f)
+{
+    sf::Vector2f size_player = { 90.f,90.f };
+    m_player.setSize(size_player);        
+    m_player.setPosition(this->origin_pos);
+    // people.setTextureRect({ 0, 0, 11, 8 });
         
-    }
+}
 
-    void Player::restart()
-    {
-        v_speed *= 0.0f;
-      //  people.setTextureRect({ 0, 0, 11, 8 });//
-        is_Alive = true;        
-        people.setPosition(this->origin_pos);
-    }
+void Player::restart()
+{
+    v_speed *= 0.0f;
+    //  people.setTextureRect({ 0, 0, 11, 8 });//
+    is_Alive = true;        
+    m_player.setPosition(this->origin_pos);
+}
 
-    void Player::inputKeyPress()
-    {
-        using Key = sf::Keyboard::Key;
-        auto keyDown = [](sf::Keyboard::Key k) {
-            return sf::Keyboard::isKeyPressed(k);
-        };
+void Player::inputKeyPress()
+{
+    using Key = sf::Keyboard::Key;
+    auto keyDown = [](sf::Keyboard::Key k) {
+        return sf::Keyboard::isKeyPressed(k);
+    };
         
-        if (keyDown(Key::A)) {
-            v_speed.x -= speed;
-        }
-        else if (keyDown(Key::D)) {
-            v_speed.x += speed;
-        }
-        else if (keyDown(Key::W)) {
-            v_speed.y -= speed;
-        }
-        else if (keyDown(Key::S)) {
-            v_speed.y += speed;
-        }
+    if (keyDown(Key::A)) {
+        v_speed.x -= speed;
     }
+    else if (keyDown(Key::D)) {
+        v_speed.x += speed;
+    }
+    else if (keyDown(Key::W)) {
+        v_speed.y -= speed;
+    }
+    else if (keyDown(Key::S)) {
+        v_speed.y += speed;
+    }
+}
 
-    void Player::update(float dt)
-    {
-        if (is_Alive) {
-            auto w = people.getGlobalBounds().width;
-            auto h = people.getGlobalBounds().height;
-            people.move(v_speed );
-            v_speed *= 0.f;
-            //if player move out of bound
+void Player::update(float dt)
+{
+    if (is_Alive) {
+        auto w = m_player.getGlobalBounds().width;
+        auto h = m_player.getGlobalBounds().height;
+        m_player.move(v_speed );
+        v_speed *= 0.f;
+        //if player move out of bound
             
-            //Left
-            if (this->people.getGlobalBounds().left <= 0.f)
-                this->people.setPosition(0.f, this->people.getGlobalBounds().top);
-            //Right
-            if (this->people.getGlobalBounds().left + this->people.getGlobalBounds().width >= 1280)
-                this->people.setPosition(1280- this->people.getGlobalBounds().width, this->people.getGlobalBounds().top);
-            //Top
-            if (this->people.getGlobalBounds().top <= 0.f) {
-                this->people.setPosition(this->people.getGlobalBounds().left, 720 - this->people.getGlobalBounds().height);
-                World::levelUp();
-            }
-            //Bottom
-            if (this->people.getGlobalBounds().top + this->people.getGlobalBounds().height >= 720)
-                this->people.setPosition(this->people.getGlobalBounds().left, 720 - this->people.getGlobalBounds().height);
+        //Left
+        if (this->m_player.getGlobalBounds().left <= 0.f)
+            this->m_player.setPosition(0.f, this->m_player.getGlobalBounds().top);
+        //Right
+        if (this->m_player.getGlobalBounds().left + this->m_player.getGlobalBounds().width >= 1280)
+            this->m_player.setPosition(1280- this->m_player.getGlobalBounds().width, this->m_player.getGlobalBounds().top);
+        //Top
+        if (this->m_player.getGlobalBounds().top <= 0.f) {
+            this->m_player.setPosition(this->m_player.getGlobalBounds().left, 720 - this->m_player.getGlobalBounds().height);
+            World::levelUp();
         }
+        //Bottom
+        if (this->m_player.getGlobalBounds().top + this->m_player.getGlobalBounds().height >= 720)
+            this->m_player.setPosition(this->m_player.getGlobalBounds().left, 720 - this->m_player.getGlobalBounds().height);
     }
+}
 
-    void Player::draw(sf::RenderTarget& target)
-    {
-        if (!is_Alive) {
-            //people.setTextureRect(death_Animation.getFrame());
-        }
-        else
-            target.draw(people);
+void Player::draw(sf::RenderTarget& target)
+{
+    if (!is_Alive) {
+        //people.setTextureRect(death_Animation.getFrame());
+    }
+    else
+        target.draw(m_player);
         
-    }
+}
 
-   
-
-
-    bool Player::isAlive() const
-    {
-        return is_Alive;
-    }   
+  
+bool Player::isAlive() const
+{
+    return is_Alive;
+}   
 
--- Crossy Road/Game/Entity/Player.h
+++ Crossy Road/Game/Entity/Player.h
@@ -5,8 +5,6 @@
 #include "../Collidable.h"
 #include <SFML/Audio.hpp>
 
-
-
     /**
         Represents the player
     */
@@ -22,7 +20,7 @@
         void update(float dt);
         void draw(sf::RenderTarget& target);           
 
-        const sf::Vector2f& getPosition() const { return people.getPosition(); };
+        const sf::Vector2f& getPosition() const { return m_player.getPosition(); };
         void onCollide(Collision& other) override { is_Alive = false; }
      
         bool isAlive() const;
@@ -30,7 +28,7 @@
     private:
         void restart();
 
-        sf::RectangleShape people;     
+        sf::RectangleShape m_player;     
         sf::Vector2f v_speed;   
         sf::Vector2f origin_pos = { 640, 720 - 10 };
         float speed = 45.f;
