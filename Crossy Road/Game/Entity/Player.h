b605eed8748c29d75c5d368bbf07e3d574fdb5b4
Phineasla <63248715+Phineasla@users.noreply.github.com>
2020-08-24 11:00:19 +0700

    Update Player.h

--- Crossy Road/Game/Entity/Player.h
+++ Crossy Road/Game/Entity/Player.h
@@ -2,41 +2,39 @@
 #define Player_h
 
 #include <SFML/Graphics.hpp>
-#include "../Collidable.h"
 #include <SFML/Audio.hpp>
+#include "../Collidable.h"
 
+/*
+	Represents the player
+*/
+class Player :public Collision
+{
+public:
+	constexpr static int WIDTH = 44;
+	constexpr static int HEIGHT = 32;
 
+	Player();
 
-    /**
-        Represents the player
-    */
-    class Player :public Collision
-    {
-    public:
-        constexpr static int WIDTH = 44;
-        constexpr static int HEIGHT = 32;
+	void inputKeyPress();
+	void update(float dt);
+	void draw(sf::RenderTarget& target);           
 
-        Player();
+	const sf::Vector2f& getPosition() const { return people.getPosition(); };
+	void onCollide(Collision& other) override { is_Alive = false; }
+	 
+	bool isAlive() const;
 
-        void inputKeyPress();
-        void update(float dt);
-        void draw(sf::RenderTarget& target);           
+private:
+	void restart();
 
-        const sf::Vector2f& getPosition() const { return people.getPosition(); };
-        void onCollide(Collision& other) override { is_Alive = false; }
-     
-        bool isAlive() const;
-
-    private:
-        void restart();
-
-        sf::RectangleShape people;     
-        sf::Vector2f v_speed;   
-        sf::Vector2f origin_pos = { 640, 720 - 10 };
-        float speed = 45.f;
-        bool is_Alive = true;        
-        sf::Texture player_texture;
-       
-    };
+	sf::RectangleShape people;     
+	sf::Vector2f v_speed;   
+	sf::Vector2f origin_pos = { 640, 720 - 10 };
+	float speed = 45.f;
+	bool is_Alive = true;        
+	sf::Texture player_texture;
+	   
+};
 
 #endif
