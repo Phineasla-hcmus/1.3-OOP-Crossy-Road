1a97adc7ebb095cdf2a75461ca681c350da10762
Phineasla <63248715+Phineasla@users.noreply.github.com>
2020-08-24 10:19:54 +0700

    revert syntax error

--- Crossy Road/Game/Entity/Player.cpp
+++ Crossy Road/Game/Entity/Player.cpp
@@ -1,62 +1,68 @@
 #include "Player.h"
 
-Player::Player() :Collision(90.f, 90.f)
-{
-    sf::Vector2f size_player = { 90.f,90.f };
-    people.setSize(size_player);
-    people.setPosition(this->origin_pos);
-    player_texture.loadFromFile("Assets\\textures\\player_sprite_2.png");
-    people.setTexture(&player_texture);
-    people.setTextureRect(sf::IntRect{ 0,192,64,64 });
-}
-
-void Player::restart()
-{
-    v_speed *= 0.0f;
-    people.setTextureRect(sf::IntRect{ 0,192,64,64 });
-    is_Alive = true;        
-    people.setPosition(this->origin_pos);
-}
-
-void Player::inputKeyPress()
-{
-
-    using Key = sf::Keyboard::Key;
-    auto keyDown = [](sf::Keyboard::Key k) {
-        return sf::Keyboard::isKeyPressed(k);
-    };
-    sf::Clock clock;
-    if (keyDown(Key::A)) {
-        clock.restart();
-        people.setTextureRect(sf::IntRect{ 0,64,64,64 });
-        /* people.setTextureRect(sf::IntRect{ 64,64,64,64 });*/
-        v_speed.x -= speed;
-        if (clock.getElapsedTime().asSeconds() > 0.01f)
-            v_speed.x = 0.f;
-    }
-    else if (keyDown(Key::D)) {
-        clock.restart();
-        people.setTextureRect(sf::IntRect{ 0,128,64,64 });
-        v_speed.x += speed;
-        if (clock.getElapsedTime().asSeconds() > 0.01f)
-            v_speed.x = 0.f;
-    }
-    else if (keyDown(Key::W)) {
-        clock.restart();
-        people.setTextureRect(sf::IntRect{ 0,192,64,64 });
-        v_speed.y -= speed;
-        if (clock.getElapsedTime().asSeconds() > 0.01f)
-            v_speed.y = 0.f;
-    }
-    else if (keyDown(Key::S)) {
-        clock.restart();
-        people.setTextureRect(sf::IntRect{ 0,0,64,64 });
-        v_speed.y += speed;
-        if (clock.getElapsedTime().asSeconds() > 0.01f)
-            v_speed.y = 0.f;
+    Player::Player() :Collision(90.f,90.f)
+    {
+        sf::Vector2f size_player = { 90.f,90.f };
+        people.setSize(size_player);        
+        people.setPosition(this->origin_pos);
+       // people.setTextureRect({ 0, 0, 11, 8 });
+        
     }
 
-}
+    void Player::restart()
+    {
+        v_speed *= 0.0f;
+      //  people.setTextureRect({ 0, 0, 11, 8 });//
+        is_Alive = true;        
+        people.setPosition(this->origin_pos);
+    }
+
+    void Player::inputKeyPress()
+    {
+        using Key = sf::Keyboard::Key;
+        auto keyDown = [](sf::Keyboard::Key k) {
+            return sf::Keyboard::isKeyPressed(k);
+        };
+        
+        if (keyDown(Key::A)) {
+            v_speed.x -= speed;
+        }
+        else if (keyDown(Key::D)) {
+            v_speed.x += speed;
+        }
+        else if (keyDown(Key::W)) {
+            v_speed.y -= speed;
+        }
+        else if (keyDown(Key::S)) {
+            v_speed.y += speed;
+        }
+    }
+
+    void Player::update(float dt)
+    {
+        if (is_Alive) {
+            auto w = people.getGlobalBounds().width;
+            auto h = people.getGlobalBounds().height;
+            people.move(v_speed );
+            v_speed *= 0.f;
+            //if player move out of bound
+            
+            //Left
+            if (this->people.getGlobalBounds().left <= 0.f)
+                this->people.setPosition(0.f, this->people.getGlobalBounds().top);
+            //Right
+            if (this->people.getGlobalBounds().left + this->people.getGlobalBounds().width >= 1280)
+                this->people.setPosition(1280- this->people.getGlobalBounds().width, this->people.getGlobalBounds().top);
+            //Top
+            if (this->people.getGlobalBounds().top <= 0.f) {
+                this->people.setPosition(this->people.getGlobalBounds().left, 720 - this->people.getGlobalBounds().height);
+                World::levelUp();
+            }
+            //Bottom
+            if (this->people.getGlobalBounds().top + this->people.getGlobalBounds().height >= 720)
+                this->people.setPosition(this->people.getGlobalBounds().left, 720 - this->people.getGlobalBounds().height);
+        }
+    }
 
 void Player::update(float dt)
 {
