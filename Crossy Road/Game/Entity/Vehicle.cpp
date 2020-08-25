a6fa0da06eaea492a5c167475dc978ecd37a60dc
Phineasla <63248715+Phineasla@users.noreply.github.com>
2020-08-25 14:15:44 +0700

    add car1.png

--- Crossy Road/Game/Entity/Vehicle.cpp
+++ Crossy Road/Game/Entity/Vehicle.cpp
@@ -1,87 +1,84 @@
 #include "Vehicle.h"
-#include "../../Framework/Util/random.h"
 
-Vehicle::Vehicle(sf::Color color, sf::Vector2f position, sf::Vector2f size) :Collision(size.x, size.y)
-{
-	this->vehicle.setPosition(position);
-	this->vehicle.setSize(size);
-	this->vehicle.setFillColor(color);
-	//this->enemy.setOutlineColor(sf::Color::Green);
-	//this->enemy.setOutlineThickness(1.f);
-}
-
-Car::Car(sf::Vector2f position) :m_active(true), Vehicle(sf::Color::White, sf::Vector2f(20.f, 20.f), sf::Vector2f(90.f, 70.f)) {
-
-	this->vehicle.setSize(sf::Vector2f(90.f, 70.f));
-
-	this->vehicle.setPosition(sf::Vector2f(position.x, position.y - this->vehicle.getSize().y / 2));
-	this->vehicle.setFillColor(sf::Color::Blue);	
-
-};
-void Car::draw(sf::RenderTarget& target) {
-	target.draw(this->vehicle);
-}
-
-Vehicle* Car::newVehicle(sf::Vector2f origin_pos)
-{
-	return new Car(origin_pos);
-}
-
-Truck::Truck(sf::Vector2f position) : m_active(true),Vehicle(sf::Color::Yellow, sf::Vector2f(15.f, 15.f), sf::Vector2f(90.f, 90.f)) {
-	this->vehicle.setSize(sf::Vector2f(90.f, 90.f));
-	this->vehicle.setPosition(sf::Vector2f(position.x, position.y - this->vehicle.getSize().y / 2));
-	this->vehicle.setFillColor(sf::Color::Yellow);
-		
-};
-void Truck::draw(sf::RenderTarget& target) {
-	target.draw(this->vehicle);
-}
-
-Vehicle* Truck::newVehicle(sf::Vector2f origin_pos)
-{
-	return new Truck(origin_pos);
-}
-	
-Bird::Bird(sf::Vector2f position) :m_active(true),Vehicle(sf::Color::Yellow, sf::Vector2f(15.f, 15.f), sf::Vector2f(100.f, 50.f)) {
-	this->vehicle.setSize(sf::Vector2f(90.f, 50.f));
-	this->vehicle.setPosition(sf::Vector2f(position.x, position.y - this->vehicle.getSize().y / 2));
-	this->vehicle.setFillColor(sf::Color::Magenta);
-};
-void Bird::draw(sf::RenderTarget& target) {
-	target.draw(this->vehicle);
-}
-
-Vehicle* Bird::newVehicle(sf::Vector2f origin_pos)
-{
-	return new Bird(origin_pos);
-}
-
-
-
-//Vehicle::Vehicle(const sf::Texture& texture, sf::Vector2f size, sf::Vector2f pos)
-//	: m_sprite(size)
-//	, m_texture(texture)
-//	, Collision(size.x, size.y)
+//Vehicle::Vehicle(sf::Color color, sf::Vector2f position, sf::Vector2f size) :Collision(size.x, size.y)
 //{
-//	m_sprite.setTexture(&m_texture);
-//	m_sprite.setPosition(pos);
+//	this->vehicle.setPosition(position);
+//	this->vehicle.setSize(size);
+//	this->vehicle.setFillColor(color);
+//	//this->enemy.setOutlineColor(sf::Color::Green);
+//	//this->enemy.setOutlineThickness(1.f);
 //}
 //
-//Vehicle::Vehicle(const sf::Texture& texture, float vehicle_length, sf::Vector2f pos)
-//	: Vehicle(texture, sf::Vector2f(vehicle_length, tile_size), pos)
-//{}
+//Car::Car(sf::Vector2f position) :m_active(true), Vehicle(sf::Color::White, sf::Vector2f(20.f, 20.f), sf::Vector2f(90.f, 70.f)) {
 //
-//const sf::Vector2f& Vehicle::getPosition() const
-//{
-//	return m_sprite.getPosition();
+//	this->vehicle.setSize(sf::Vector2f(90.f, 70.f));
+//
+//	this->vehicle.setPosition(sf::Vector2f(position.x, position.y - this->vehicle.getSize().y / 2));
+//	this->vehicle.setFillColor(sf::Color::Blue);	
+//
+//};
+//void Car::draw(sf::RenderTarget& target) {
+//	target.draw(this->vehicle);
 //}
 //
-//void Vehicle::draw(sf::RenderTarget& render) const
+//Vehicle* Car::newVehicle(sf::Vector2f origin_pos)
 //{
-//	render.draw(m_sprite);
+//	return new Car(origin_pos);
 //}
 //
-//void Vehicle::move(float speed, float dt_time, DRoad::direction dir)
-//{
-//	m_sprite.move(speed * dt_time * (int)dir, 0);
+//Truck::Truck(sf::Vector2f position) : m_active(true),Vehicle(sf::Color::Yellow, sf::Vector2f(15.f, 15.f), sf::Vector2f(90.f, 90.f)) {
+//	this->vehicle.setSize(sf::Vector2f(90.f, 90.f));
+//	this->vehicle.setPosition(sf::Vector2f(position.x, position.y - this->vehicle.getSize().y / 2));
+//	this->vehicle.setFillColor(sf::Color::Yellow);
+//		
+//};
+//void Truck::draw(sf::RenderTarget& target) {
+//	target.draw(this->vehicle);
 //}
+//
+//Vehicle* Truck::newVehicle(sf::Vector2f origin_pos)
+//{
+//	return new Truck(origin_pos);
+//}
+//	
+//Bird::Bird(sf::Vector2f position) :m_active(true),Vehicle(sf::Color::Yellow, sf::Vector2f(15.f, 15.f), sf::Vector2f(100.f, 50.f)) {
+//	this->vehicle.setSize(sf::Vector2f(90.f, 50.f));
+//	this->vehicle.setPosition(sf::Vector2f(position.x, position.y - this->vehicle.getSize().y / 2));
+//	this->vehicle.setFillColor(sf::Color::Magenta);
+//};
+//void Bird::draw(sf::RenderTarget& target) {
+//	target.draw(this->vehicle);
+//}
+//
+//Vehicle* Bird::newVehicle(sf::Vector2f origin_pos)
+//{
+//	return new Bird(origin_pos);
+//}
+
+Vehicle::Vehicle(const sf::Texture& texture, sf::Vector2f size, sf::Vector2f pos)
+	: m_sprite(size)
+	, m_texture(texture)
+	, Collision(size.x, size.y)
+{
+	m_sprite.setTexture(&m_texture);
+	m_sprite.setPosition(pos);
+}
+
+Vehicle::Vehicle(const sf::Texture& texture, float vehicle_length, sf::Vector2f pos)
+	: Vehicle(texture, sf::Vector2f(vehicle_length, tile_size), pos)
+{}
+
+const sf::Vector2f& Vehicle::getPosition() const
+{
+	return m_sprite.getPosition();
+}
+
+void Vehicle::draw(sf::RenderTarget& render) const
+{
+	render.draw(m_sprite);
+}
+
+void Vehicle::move(float speed, float dt_time, DRoad::direction dir)
+{
+	m_sprite.move(speed * dt_time * (int)dir, 0);
+}
--- Crossy Road/Game/Entity/Vehicle.h
+++ Crossy Road/Game/Entity/Vehicle.h
@@ -7,74 +7,75 @@
 #include "Player.h"
 #include "../Collidable.h"
 
-class Vehicle :public Collision {
-protected:
-	sf::RectangleShape vehicle;
-	friend class DRoad;
-public:
-	Vehicle(sf::Color color = sf::Color::White, sf::Vector2f position = { 0.f,0.f }, sf::Vector2f size = { 90.f,90.f });
-	virtual void onCollide(Collision& other) = 0;
-	virtual const sf::Vector2f& getPosition() const = 0;
-	virtual void draw(sf::RenderTarget& target) = 0;
-};
-class Car :public Vehicle {
-private:
-	bool m_active;
-public:
-	Car(sf::Vector2f position = { 0.f,0.f });
-	virtual void draw(sf::RenderTarget& target);
-	void onCollide(Collision& other) {
-		m_active = false;
-	}
-	const sf::Vector2f& getPosition() const {
-		return vehicle.getPosition();
-	}
-	static Vehicle* newVehicle(sf::Vector2f origin_pos);
-};
-
-class Truck :public Vehicle {
-private:
-	bool m_active;
-public:
-	Truck(sf::Vector2f position = { 0.f,0.f });
-	void onCollide(Collision& other) {
-		m_active = false;
-	}
-	const sf::Vector2f& getPosition() const {
-		return vehicle.getPosition();
-	}
-	virtual void draw(sf::RenderTarget& target);
-	static Vehicle* newVehicle(sf::Vector2f origin_pos);
-
-};
-
-class Bird :public Vehicle {
-private:
-	bool m_active;
-public:
-	Bird(sf::Vector2f position = { 0.f,0.f });
-	void onCollide(Collision& other) {
-		m_active = false;
-	}
-	const sf::Vector2f& getPosition() const {
-		return vehicle.getPosition();
-	}
-	void draw(sf::RenderTarget& target);
-	static Vehicle* newVehicle(sf::Vector2f origin_pos);
-};
-
-//class Vehicle : public Collision {
-//private:
-//	sf::RectangleShape m_sprite;
-//	const sf::Texture& m_texture;
+//class Vehicle :public Collision {
+//protected:
+//	sf::RectangleShape vehicle;
+//	friend class DRoad;
 //public:
-//	Vehicle(const sf::Texture&, sf::Vector2f size = { tile_size,tile_size }, sf::Vector2f pos = { 0,0 });
-//	Vehicle(const sf::Texture&, float vehicle_length, sf::Vector2f pos = { 0,0 });
-//
-//	const sf::Vector2f& getPosition() const;
-//	void draw(sf::RenderTarget&) const;
-//	void move(float speed, float dt_time, DRoad::direction);
+//	Vehicle(sf::Color color = sf::Color::White, sf::Vector2f position = { 0.f,0.f }, sf::Vector2f size = { 90.f,90.f });
+//	virtual void onCollide(Collision& other) = 0;
+//	virtual const sf::Vector2f& getPosition() const = 0;
+//	virtual void draw(sf::RenderTarget& target) = 0;
 //};
+//class Car :public Vehicle {
+//private:
+//	bool m_active;
+//public:
+//	Car(sf::Vector2f position = { 0.f,0.f });
+//	virtual void draw(sf::RenderTarget& target);
+//	void onCollide(Collision& other) {
+//		m_active = false;
+//	}
+//	const sf::Vector2f& getPosition() const {
+//		return vehicle.getPosition();
+//	}
+//	static Vehicle* newVehicle(sf::Vector2f origin_pos);
+//};
+//
+//class Truck :public Vehicle {
+//private:
+//	bool m_active;
+//public:
+//	Truck(sf::Vector2f position = { 0.f,0.f });
+//	void onCollide(Collision& other) {
+//		m_active = false;
+//	}
+//	const sf::Vector2f& getPosition() const {
+//		return vehicle.getPosition();
+//	}
+//	virtual void draw(sf::RenderTarget& target);
+//	static Vehicle* newVehicle(sf::Vector2f origin_pos);
+//
+//};
+//
+//class Bird :public Vehicle {
+//private:
+//	bool m_active;
+//public:
+//	Bird(sf::Vector2f position = { 0.f,0.f });
+//	void onCollide(Collision& other) {
+//		m_active = false;
+//	}
+//	const sf::Vector2f& getPosition() const {
+//		return vehicle.getPosition();
+//	}
+//	void draw(sf::RenderTarget& target);
+//	static Vehicle* newVehicle(sf::Vector2f origin_pos);
+//};
+
+class Vehicle : public Collision {
+private:
+	sf::RectangleShape m_sprite;
+	const sf::Texture& m_texture;
+public:
+	Vehicle(const sf::Texture&, sf::Vector2f size = { tile_size,tile_size }, sf::Vector2f pos = { 0,0 });
+	Vehicle(const sf::Texture&, float vehicle_length, sf::Vector2f pos = { 0,0 });
+
+	const sf::Vector2f& getPosition() const;
+	void draw(sf::RenderTarget&) const;
+	void move(float speed, float dt_time, DRoad::direction);
+};
+
 
 template<typename T>
 std::unique_ptr<Vehicle> new_vehicle() 
