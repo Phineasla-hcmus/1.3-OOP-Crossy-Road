3967304f6cb911a4c00dd0d5ba93e46bb1661093
MinhLe2512 <63902542+MinhLe2512@users.noreply.github.com>
2020-08-28 11:32:33 +0700

    changed_tileset

--- Crossy Road/Game/world.cpp
+++ Crossy Road/Game/world.cpp
@@ -5,13 +5,10 @@ World::World(const textureLookup& lookup)
 	, m_background(&asset::texture().get("road_textures", "png"), sf::Vector2u(20, 20))//size base on the tileset.png
 	, m_rand()
 {
-<<<<<<< Updated upstream
-	//render background map (top to bottom)
-	const std::vector<unsigned> tile_map = { 0,1,0,1,0,1,0,1 };
-=======
+
 	//lookup background
 	const std::vector<unsigned> tile_map = { 1,0,1,0,1,0,1,0 };
->>>>>>> Stashed changes
+
 	//render y_tiles+1 to fill whole screen
 	m_background.setMapSize(x_tiles+1, y_tiles);
 	//scale the texture to tile_size (90)
