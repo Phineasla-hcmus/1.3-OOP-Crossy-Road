55b78da240c908766a421d661a2669fa1fdaf14a
Phúc La <phucla@outlook.com>
2020-09-07 23:41:20 +0700

    tweak in animation.h

--- Crossy Road/Framework/Util/animation.h
+++ Crossy Road/Framework/Util/animation.h
@@ -3,23 +3,23 @@
 #include<SFML/Graphics.hpp>
 #include<vector>
 class animation {
+public:
+	animation(size_t frame_width, size_t frame_height);
+	void add_frame(sf::Time delay, size_t row, size_t col = 0);
+	const sf::IntRect nextFrame();
+	const sf::IntRect getFrame(size_t);
+	void	reset();
+	size_t	totalFrame();
 private:
 	struct frame {
-        sf::IntRect bounds; //The area in a m_texture of the frame
-        sf::Time    delay;  //Time delay to next frame
-		frame(const sf::IntRect& bounds, sf::Time delay);
+		sf::Vector2i	pos;	//The position of the frame in a texture
+		sf::Time		delay;  //Time delay to next frame
+		frame(const sf::Vector2i pos, sf::Time delay);
+		frame(int x, int y, sf::Time delay);
 	};
 	sf::Clock			m_timer;		//For progressing the animation
 	std::vector<frame>	m_frames;		//List of animation frames
 	size_t				m_frame_idx = 0;//The index of the current active frame
-	const size_t		FRAME_WIDTH;
-	const size_t		FRAME_HEIGHT;
-public:
-	//frame_width(file_width), frame_height(file_height)
-	animation(size_t frame_width, size_t frame_height);
-	void add_frame(sf::Time delay, size_t row, size_t col = 0);
-	const sf::IntRect& nextFrame();
-	const sf::IntRect& getFrame(size_t);
-	size_t totalFrame();
+	const sf::Vector2i	m_frame_size;
 };
 #endif // !_animation_h
