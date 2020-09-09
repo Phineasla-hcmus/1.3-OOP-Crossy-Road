6ea5760450f5720d85b4cee480cd671c085adc79
Truong19clc5 <nguyentony3867@gmail.com>
2020-09-09 20:37:27 +0700

    update

--- Crossy Road/Game/State/state_load.cpp
+++ Crossy Road/Game/State/state_load.cpp
@@ -6,6 +6,8 @@ state_load::state_load(Game& game)
 	, load_menu(game.get_window(), 300, &asset::texture().get("blue_panel", "png"))
 	, load_inf()
 {
+	background2.setTexture(&asset::texture().get("background2","png"));
+	background2.setSize({1280,720 });
 
 	background.setPosition({ 440,50 });
 	background.setSize({ 400,100 });
@@ -16,7 +18,7 @@ state_load::state_load(Game& game)
 	fail_label.setCharacterSize(50);
 	fail_label.setFillColor(sf::Color::White);
 	fail_label.setOutlineColor(sf::Color::Black);
-	fail_label.setPosition({ 520,60 });
+	fail_label.setPosition({ 490,60 });
 	fail_label.setString(warning);
 
 	auto Load_Textbox = makeTextBox(name);
@@ -59,7 +61,7 @@ void state_load::update(sf::Time delta_time)
 
 void state_load::draw(sf::RenderTarget& renderer)
 {
-	
+	renderer.draw(background2);
 	load_menu.render(renderer);
 	if (is_fail)
 	{
